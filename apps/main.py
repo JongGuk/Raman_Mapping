57dc71163b8df316b403e3b151699cb16669c6c3
Jong-Guk Ahn <jongguk.ahn@gmail.com>
2021-03-05 17:47:47 +0900

    Data slicing by index and specific range plot

--- apps/main.py
+++ apps/main.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from tkinter import filedialog
+import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas import DataFrame
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 class RamanGUI:
     def __init__(self):
@@ -19,13 +23,8 @@ class RamanGUI:
         self.btn = Button(self.root, text="OK")
         self.btn.pack()
         
-        x_values = [1, 2, 3, 4, 5]
-        y_values = [3, 3, 2, 6, 4]
-        plt.plot(x_values, y_values)
-        plt.show()
-
-        self.text = Text(self.root)
-        self.text.pack()
+        # plt.plot(x_values, y_values)
+        # plt.show()
 
         self.root.config(menu=menubar)
         self.root.mainloop()
@@ -33,10 +32,42 @@ class RamanGUI:
     def Load(self):
         filename = filedialog.askopenfilename(initialdir="/", title="Selecte raw file")
         print(filename)
-        data = open(filename,'rt')
-        self.text.delete('1.0', END)
-        self.text.insert(END,data.read())
+        data = pd.read_csv(filename)
+        print(data)
+        df = DataFrame(data) # data 로부터 데이터 프레임 만듦
+        df1=df.iloc[[60],3:1027]
+        print(df1)
+        df1T=df1.T
+        
+        root= tk.Tk() # tkinter 로 창 띄우기
 
+        figure1 = plt.Figure(figsize=(5,6), dpi=100) # 그래프 띄울 창 사이즈
+        ax = figure1.add_subplot(111) # 그래프 plot 및 x,y축 범위 조절 (범위 지정 생략하면 auto)
+        ax.set_title('Raman spectrum at selected point')
+
+        line = FigureCanvasTkAgg(figure1, root) # Figure 그려서 root에 표시 
+        line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH) # pack 에서 그래프를 좌측정렬/채우기 등 설정
+
+        #df.plot(~~~) # df.행이름 으로 특정 행 선택 가능
+        df1T.plot(kind='line', ax=ax, color='r', marker='*', fontsize=10)
+        
+        Rdf1T=df1T['533.1':'533.7']
+        
+        figure2 = plt.Figure(figsize=(5,6), dpi=100) # 그래프 띄울 창 사이즈
+        ax2 = figure2.add_subplot(111)
+
+        line = FigureCanvasTkAgg(figure2, root) # Figure 그려서 root에 표시 
+        line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH) # pack 에서 그래프를 좌측정렬/채우기 등 설정
+
+        Rdf1T.plot(kind='line', ax=ax2, color='b', marker='o', fontsize=10)
+
+
+
+
+        root.mainloop() #새로고침 
+
+
+    # def Sel_pt(self):
 
 # Load()
 
--- apps/plot.py
+++ apps/plot.py
@@ -3,28 +3,24 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-data = {'Raman_Shift': [-464, -460, -455, -450, -445], 
+data = {'Raman Shift': [-464, -460, -455, -450, -445], 
          'Intensity1': [745, 752, 746, 740, 750], 'Intensity2': [734, 745, 768, 763, 755]
         } # 데이터 짤라온것 받아오기
 
-df = DataFrame(data, columns=['Raman_Shift','Intensity1','Intensity2'])#, index='Raman_Shift') # 
-df.set_index('Raman_Shift', inplace=True)
+df = DataFrame(data) # data 로부터 데이터 프레임 만듦
+df.set_index('Raman Shift', inplace=True) # 만들어진 데이터 프레임 중, Raman_Shift 항목을 x 축으로 지정
 
-root= tk.Tk() # tkinter 로 띄우기
+root= tk.Tk() # tkinter 로 창 띄우기
 
 figure = plt.Figure(figsize=(5,4), dpi=100) # 그래프 띄울 창 사이즈
-ax = figure.add_subplot(111) # 그래프 plot + x,y축 범위 조절
+ax = figure.add_subplot(111) # 그래프 plot 및 x,y축 범위 조절 (범위 지정 생략하면 auto)
+ax.set_title('Raman spectrum at selected point')
 
 line = FigureCanvasTkAgg(figure, root) # Figure 그려서 root에 표시 
 line.get_tk_widget().pack() # pack 에서 그래프를 좌측정렬/채우기 등 설정
 
-#df = df[['Intensity1','Intensity2']]
-# df = df['Intensity']
-# df.plt(x="Raman_Shift", y="Intensity", kind="line", color="r", marker="o", fontsize=10)
-df.plot(kind='line', ax=ax, color='r', marker='o', fontsize=10)
-#df.plot.line()
+#df.plot(~~~) # df.행이름 으로 특정 행 선택 가능
+df.Intensity2.plot(kind='line', ax=ax, color='r', marker='o', fontsize=10)
 
-#ax.set_title('Year Vs. Unemployment Rate')
-
-root.mainloop() # 새로고침 
+root.mainloop() #새로고침 
 
